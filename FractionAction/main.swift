f903f3df4bc2c37eb6df2d9d0ea503569da30270
Da, Annie <ada@lcs-mba3107.lcs.on.ca>
2018-05-04 07:34:28 -0400

    loop to check numerator and denumerator completed, not finished

--- FractionAction/main.swift
+++ FractionAction/main.swift
@@ -10,10 +10,64 @@ import Foundation
 
 // INPUT
 // Collect and filter user input here
+var numerator = 0
+var denumerator = 0
 
 
 // PROCESS
-// Implement the primary logic of the problem here
+
+//check numerator
+while 1 == 1 {
+    print("numerator")
+    
+    // Waiting for input
+    guard let integerNumerator = readLine() else {
+        continue
+        // Check for nil
+    }
+    //test 2:
+    guard let givenInput = Int(integerNumerator)else{
+        continue
+    }
+    //test 3:
+    if givenInput < 0 {
+        continue
+    }
+    numerator = givenInput
+    break
+}
+
+print(numerator)
+
+//check denumerator
+while 1 == 1 {
+    print("denomator")
+    
+    //wait for the input
+    guard let givenInteger = readLine() else{
+        //check for nil
+        continue
+    }
+    //test 2:
+    guard let integerDenomator = Int(givenInteger) else{
+        //check for integer
+        continue
+    }
+    //test 3:
+    if integerDenomator < 1 {
+        continue
+    }
+    denumerator = integerDenomator
+    break
+}
+
+print(denomnator)
+
+//quotient and reminder
+var quotient = numerator / denumerator
+var reminder = numerator % denumerator
+
+
 
 
 // OUTPUT
